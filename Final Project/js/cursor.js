30fd4ad35511d676327da8ee4846e2b0bcccc755
NurzhauganovA <90419990+NurzhauganovA@users.noreply.github.com>
2021-12-21 03:14:43 +0600

    Delete Final Project directory

--- Final Project/js/cursor.js
+++
@@ -1,8 +0,0 @@
-let mouseCursor = document.getElementById('cursor');
-
-window.addEventListener('mousemove', cursor);
-
-function cursor(event) {
-    mouseCursor.style.top = event.pageY + "px";
-    mouseCursor.style.left = event.pageX + "px";
-}
